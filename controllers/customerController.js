a7b2f29700b2f86bf03def1d49274cfd25d30d45
RajaRehman054 <abdurrehman283560@gmail.com>
2023-06-13 16:27:02 +0500

    For larger pictures

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -213,12 +213,10 @@ exports.getPizzaBurgerRestaurant = asyncHandler(async (req, res) => {
 	const restaurants = await FoodCategory.find({
 		$or: [{ title: 'pizza' }, { title: 'burger' }],
 	}).select('restaurant -_id');
-	console.log(restaurants);
 	var ids = [];
 	for (let index = 0; index < restaurants.length; index++) {
 		ids.push(restaurants[index].restaurant);
 	}
-	console.log(ids);
 	const finalData = await Restaurant.find({ _id: { $in: ids } });
 	res.status(200).json({ restaurants: finalData });
 });
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(process.env.PORT, () => {
 connection.connectDB();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
